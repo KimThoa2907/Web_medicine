70eee3d25e6182cc6f648e0e61df8319cca9bb80
LAPTOP-MTGPP3N7\Admin <phanthikimthoa.ltp@gmail.com>
2023-03-27 17:24:09 +0700

    commit first

---
+++ pages/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width= device-width, initial= scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <title>Web bán thuốc</title>
+   
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <p> this is header</p>
+        </div>
+         
+        <div class="menu">
+             <p>menu</p>
+        </div>
+         
+        <div id="main">
+            <p>main</p>
+        </div>
+          
+        <div class="footer">
+             <p>footer</p>
+        </div>
+    </div>
+</body>
+
+</html>
