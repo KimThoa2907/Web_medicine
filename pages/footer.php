3ffdfa95a71f5f9a19035cde0760717543efbd0d
LAPTOP-MTGPP3N7\Admin <phanthikimthoa.ltp@gmail.com>
2023-03-28 15:26:57 +0700

    commit third

---
+++ pages/footer.php
@@ -0,0 +1,6 @@
+<div class="clear"></div>
+            <div class="footer">
+                <p class="footer_copyright"> Công ty KT Medicine</p>
+                <p class="footer_copyright"> Địa chỉ: 475A Điện Biên Phủ, phường 25, quận Bình Thạnh, TP.Hồ Chí Minh</p>
+                <p class="footer_copyright"> Điện thoại: 0367342469. Email: phanthikimthoa.ltp@gmail.com</p>
+</div>
---
+++ pages/sidebar/sidebar.php
@@ -0,0 +1,13 @@
+<div class="sidebar">
+                <ul class="list_sidebar">
+                    <li><a href="index.php">Dược phẩm</a></li>
+                    <li><a href="#">Chăm sóc sức khỏe</a></li>
+                    <li><a href="#">Chăm sóc cá nhân</a></li>
+                    <li><a href="#">Sản phẩm tiện lợi</a></li>
+                    <li><a href="#">Thực phẩm chức năng</a></li>   
+                    <li><a href="index.php">Mẹ và bé</a></li>
+                    <li><a href="#">Chăm sóc sắc đẹp</a></li>
+                    <li><a href="#">Thiết bị y tế</a></li>
+                    
+            </ul>
+            </div>
